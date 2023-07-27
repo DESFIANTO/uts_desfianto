724ffdc62a416dd179526d1a5a9f3f8573fe257b
fiandesfianto@gmail.com <fiandesfianto@gmail.com>
2023-07-28 03:47:47 +0700

    Commit kedua

--- lib/profil.dart
+++ lib/profil.dart
@@ -1,25 +1,58 @@
 import 'package:flutter/material.dart';
 
-
 class Profil extends StatelessWidget {
-  const Profil({super.key});
+  const Profil({Key? key}) : super(key: key);
+
+  final String name = "Desfianto";
+  final String email = "fiandesfianto@gmail.com";
+  final String address = "Lintas Selatan";
+  final String phoneNumber = "123-456-7890";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Profil"),
-       backgroundColor: Colors.grey[700],
+        backgroundColor: Colors.grey[700],
       ),
       backgroundColor: Colors.grey[500],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(" "),
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/profile_picture.png'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              name,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              email,
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 8),
+            Text(
+              address,
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 8),
+            Text(
+              phoneNumber,
+              style: TextStyle(fontSize: 18),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Profil(),
+  ));
 }
