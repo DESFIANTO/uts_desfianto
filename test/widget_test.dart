d6c7aa26e61f95ae3aeae6ae54c8c26aa679a335
fiandesfianto@gmail.com <fiandesfianto@gmail.com>
2023-07-28 13:56:39 +0700

    Commit kedua

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:uts_desfianto/halamanutama.dart';
+// ignore: unused_import
 import 'package:uts_desfianto/main.dart';
 
 void main() {
